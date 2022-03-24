af91b364243341a87d0f3d654e648a7786b449d4
Logan Parke <logan.c.parke@gmail.com>
2022-03-23 18:56:48 -0700

    sample readme added

--- Develop/dist/README.md
+++ Develop/dist/README.md
@@ -1,13 +1,20 @@
 
-    # project name: project 1
-    ## project description: this is my project
-    ## installation instructions: install it
-    ## Usage information: use it
-    ## contribution guidelines: contribute
-    ## test instructions: test it
-    ## Questions: question?
-     - link to my git hub -> github.com/loganparke
-     - if you have questions, send me and email at logan.c.parke@gmail.com
-     ## License: this application is covered under the MIT license
-    ![APM](https://img.shields.io/apm/l/vim-mode)
-    
+# project name: 
+Generate Readme
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+## project description: 
+this project generates a readme.md file based upon user input from the terminal
+## installation instructions: 
+Download the code from git hub and install node to use application
+## Usage information: 
+run the application from the terminal with node.js
+## contribution guidelines: 
+any and all constribution guidelines
+## test instructions: 
+information on how to test the applications
+## Questions: 
+questions about the app/project?
+    - link to my git hub -> [github.com/Loganparke] (github.com/Loganparke)
+    - if you have questions, send me and email at logan.c.parke@gmail.com
+## License: 
+this application is covered under the Mozilla license
--- Develop/utils/page-template.js
+++ Develop/utils/page-template.js
@@ -1,18 +1,68 @@
+
 const generateReadme = (userInput) => {
-    console.log('generateReadme text funciton');
+    var licenseName = userInput.license;
+    switch (userInput.license) {
+        case 'Apache':
+            userInput.license ='[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            console.log(userInput.license);
+            break;
+        case 'Boost':
+            userInput.license ='[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+            console.log(userInput.license);
+            break;
+        case 'BSD':
+            userInput.license ='[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+            console.log(userInput.license);
+            break;
+        case 'Eclipse':
+            userInput.license ='[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+            console.log(userInput.license);
+            break;
+        case 'GNU':
+            userInput.license ='[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            console.log(userInput.license);
+            break;
+        case 'IBM':
+            userInput.license ='[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+            console.log(userInput.license);
+            break;
+        case 'ISC':
+            userInput.license ='[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+            console.log(userInput.license);
+            break;
+        case 'MIT':
+            userInput.license ='[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            console.log(userInput.license);
+            break;
+        case 'Mozilla':
+            userInput.license ='[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+            console.log(userInput.license);
+            break;
+        default:
+        console.log('none');
+    
+    };
+
     return `
-    # project name: ${userInput.project}
-    ## project description: ${userInput.description}
-    ## installation instructions: ${userInput.installation}
-    ## Usage information: ${userInput.usage}
-    ## contribution guidelines: ${userInput.contribution}
-    ## test instructions: ${userInput.test}
-    ## Questions: ${userInput.questions}
-     - link to my git hub -> github.com/${userInput.github}
-     - if you have questions, send me and email at ${userInput.email}
-     ## License: this application is covered under the ${userInput.license} license
-    ![APM](https://img.shields.io/apm/l/vim-mode)
-    `
+# project name: ${userInput.project}
+${userInput.license}
+## project description: 
+${userInput.description}
+## installation instructions: 
+${userInput.installation}
+## Usage information: 
+${userInput.usage}
+## contribution guidelines: 
+${userInput.contribution}
+## test instructions: 
+${userInput.test}
+## Questions: 
+${userInput.questions}
+    - link to my git hub -> [github.com/${userInput.github}] (github.com/${userInput.github})
+    - if you have questions, send me and email at ${userInput.email}
+## License: 
+this application is covered under the ${licenseName} license
+`
 };
 
 module.exports = generateReadme;
